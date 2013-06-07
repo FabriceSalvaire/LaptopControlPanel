3976ba6dc8f86c2e8d35d4edd6082f450bc55e7c
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2013-06-07 10:51:08 +0200

    Added battery charge thresholds to GUI

--- LaptopControlPanel/GUI/Pages/PowerSourcePage.py
+++ LaptopControlPanel/GUI/Pages/PowerSourcePage.py
@@ -7,12 +7,17 @@
 
 ####################################################################################################
 
+import subprocess
+import re
+import sys
+
 from PyQt4 import QtGui
 
 ####################################################################################################
 
 from .Page import PageBase
 from LaptopControlPanel.System.PowerSource import PowerSources
+import LaptopControlPanel.Config.Config as Config
 
 ####################################################################################################
 
@@ -37,6 +42,18 @@ class PowerSourcePage(PageBase):
         self._power_sources = PowerSources()
 
         self.refresh()
+        self.refresh_battery_charge_threshold()
+
+        self._init_connection()
+
+    ##############################################
+
+    def _init_connection(self):
+
+        form = self._form
+
+        form.always_charge_battery_check_box.stateChanged.connect(self._always_charge_battery_check_box_changed)
+        form.apply_battery_charge_thresholds_push_button.clicked.connect(self._apply_battery_charge_thresholds)
 
     ##############################################
 
@@ -55,6 +72,80 @@ class PowerSourcePage(PageBase):
         form.battery_status_label.setText(battery.status)
         form.battery_capacity_label.setText('%u %%' % battery.capacity)
         form.battery_cycle_count_label.setText(str(battery.cycle_count))
+
+    ##############################################
+
+    def refresh_battery_charge_threshold(self):
+
+        form = self._form
+
+        try:
+            start_threshold, stop_threshold = self._get_battery_charge_threshold()
+            form = self._form
+            if start_threshold == 0 and stop_threshold == 0:
+                start_threshold, stop_threshold = Config.Battery.default_charge_thresholds
+                always_charge_battery = True
+            else:
+                always_charge_battery = False
+            form.charge_thresholds_group_box.setEnabled(not always_charge_battery)
+            form.always_charge_battery_check_box.setChecked(always_charge_battery)
+            form.start_threshold_spin_box.setValue(start_threshold)
+            form.stop_threshold_spin_box.setValue(stop_threshold)
+            form.error_message_label.hide()
+        except:
+            form.battery_charge_settings_group_box.setEnabled(False)
+
+    ##############################################
+
+    def _set_battery_charge_threshold(self, start_threshold, stop_threshold):
+
+        try:
+            subprocess.check_output(['battery-control', '--gui',
+                                     'set-threshold', '--start=%u' %start_threshold, '--stop=%u' % stop_threshold])
+        except subprocess.CalledProcessError as exception:
+            raise NameError("Set threshold call failed:\n" + exception.output)
+
+    ##############################################
+
+    def _get_battery_charge_threshold(self):
+
+        try:
+            output = subprocess.check_output(['battery-control', '--gui', 'get-threshold'])
+        except subprocess.CalledProcessError as exception:
+            print exception.output
+            raise NameError("Get threshold call failed:\n" + exception.output)
+        # {"start_threshold": 39, "stop_threshold": 80}
+        prefix = '{"start_threshold": '
+        location = output.find(prefix)
+        found = False
+        if location != -1:
+            match = re.match(r'^{"start_threshold": (\d+), "stop_threshold": (\d+)}',
+                             output[location:])
+            if match is not None:
+                start_threshold, stop_threshold = [int(x) for x in match.groups()]
+                found = True
+        if not found:
+            raise NameError("Couldn't found thresholds in:\n" + output)
+
+        return start_threshold, stop_threshold
+
+    ##############################################
+
+    def _always_charge_battery_check_box_changed(self, state):
+
+        self._form.charge_thresholds_group_box.setEnabled(not state)
+        if state:
+            self._set_battery_charge_threshold(0, 0)
+
+    ##############################################
+
+    def _apply_battery_charge_thresholds(self):
+
+        start_threshold, stop_threshold = [spin_box.value() for spin_box in
+                                           self._form.start_threshold_spin_box,
+                                           self._form.stop_threshold_spin_box
+                                           ]
+        self._set_battery_charge_threshold(start_threshold, stop_threshold)
 
 ####################################################################################################
 # 
--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,6 @@
 ####################################################################################################
 
+append_to_path_if_not $PWD/bin
 append_to_ld_library_path_if_not ${PWD}/build/lib.linux-x86_64-2.7
 append_to_python_path_if_not ${PWD}
 append_to_python_path_if_not ${PWD}/third-party
